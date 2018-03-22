115dcffb19d2a85265d00bac3438c83911477092
Postaron <florent.denef@gmail.com>
2018-03-22 09:51:42 +0100

    Début code.

--- Set.cpp
+++ Set.cpp
@@ -1,5 +1,1 @@
-//
-// Created by postaron on 22/03/18.
-//
 
-#include "Set.hpp"
--- Set.hpp
+++ Set.hpp
@@ -3,10 +3,30 @@
 
 /**
  * Aller, c'est partiiii :D
+ * Construction d'abord sans template, test ? go template : debug.
+ * Implémentation par un arbre rouge-noir (snif :'()
  */
 class set {
 private:
+	typedef int value_type;
+	typedef int* pointer;
+	typedef value_type& reference;
+	typedef enum {
+		noir, rouge
+	} couleur;
 
+	/**
+	 * Structure de nœud pour l'arbre (en interne pour éviter de s'en servir en dehors de la classe)
+	 */
+	typedef struct {
+		value_type value;
+		couleur color;
+		pointer parent;
+		pointer gauche;
+		pointer droit;
+	} node;
+
+public:
 };
 
 
