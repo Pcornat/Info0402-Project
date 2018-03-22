f93ab994bd83b8dfc868b8eaec82b703c18449fb
Postaron <florent.denef@gmail.com>
2018-03-22 09:03:50 +0100

    init

---
+++ Set.cpp
@@ -0,0 +1,5 @@
+//
+// Created by postaron on 22/03/18.
+//
+
+#include "Set.hpp"
---
+++ Set.hpp
@@ -0,0 +1,12 @@
+#ifndef PROJET_SET_HPP
+#define PROJET_SET_HPP
+
+/**
+ * Aller, c'est partiiii :D
+ */
+class set {
+
+};
+
+
+#endif //PROJET_SET_HPP
